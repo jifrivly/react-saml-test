3cc31e8ebf4bf6a1584281f62ea149fa1a981670
SMH JIFRI <jifrivly@gmail.com>
2023-02-17 15:57:16 +0530

    login and .env added

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,59 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { VITE_API_BASE_PATH } = import.meta.env;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loginInProgress, setLoginInProgress] = useState(false);
+  const [loginError, setLoginError] = useState<string | null>(null);
+
+  const handleLogin = async () => {
+    try {
+      setLoginInProgress(true);
+      setLoginError(null);
+      const response = await fetch(VITE_API_BASE_PATH + "/api/saml/login", {
+        method: "POST",
+      });
+      const data = await response.json();
+      if (data) {
+        setIsLoggedIn(true);
+        window.location.href = data.redirectUrl;
+      } else {
+        setLoginError("Login Error occurred");
+      }
+      setLoginInProgress(false);
+    } catch (error) {
+      setLoginInProgress(false);
+      setLoginError("Login Error occurred - " + error);
+      console.error(error);
+    }
+  };
+
+  const handleLogout = async () => {
+    try {
+      await fetch(VITE_API_BASE_PATH + "/api/saml/logout", { method: "POST" });
+      setIsLoggedIn(false);
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      {/* <div>
+        <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div> */}
+      <h1>SAML + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {isLoggedIn === false ? (
+          <button onClick={handleLogin}>SAML Login</button>
+        ) : (
+          <button onClick={handleLogout}>SAML Logout</button>
+        )}
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {loginInProgress ? "Login..." : loginError ? loginError : ""}
       </p>
     </div>
   );
